47ae713e60ffb3eab1c2035df89e0f6c252fa3cb
noam987 <noam@arie11.com>
2020-01-20 18:14:46 -0800

    GOT SWERVE WORKING

--- code/src/main/java/frc/robot/Robot.java
+++ code/src/main/java/frc/robot/Robot.java
@@ -12,6 +12,7 @@ import com.google.inject.Injector;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.DrivetrainControllerCommand;
@@ -96,6 +97,7 @@ public class Robot extends TimedRobot {
     CommandScheduler.getInstance().run();
 
     _robot.robotPeriodic();
+    
   }
 
   /**
