b52f3ddb86e256e68950364e0d05d656bc298013
delaynieym <43792441+delaynieym@users.noreply.github.com>
2020-01-28 08:01:30 -0800

    Tia/controller deadzones (#19)
    * added deadzone code
    
    * commented fieldrelative
    
    * wrote a comment for deadzone method.
    
    added Math.abs to deadzone logic
    
    Co-authored-by: TiaSinghania <59597284+TiaSinghania@users.noreply.github.com>

--- code/src/main/java/frc/robot/commands/DrivetrainControllerCommand.java
+++ code/src/main/java/frc/robot/commands/DrivetrainControllerCommand.java
@@ -37,10 +37,23 @@ public class DrivetrainControllerCommand extends TraceableCommand {
     @Override
     public void execute() {
         super.execute();
-        _drivetrain.move(_controller.getY(Hand.kLeft), _controller.getX(Hand.kLeft), _controller.getX(Hand.kRight), _controller.getBumper(Hand.kRight)); // theta
+        _drivetrain.move(deadZones(_controller.getY(Hand.kLeft), 0.1), deadZones(_controller.getX(Hand.kLeft), 0.1), deadZones(_controller.getX(Hand.kRight), 0.1), _controller.getBumper(Hand.kRight)); // theta
                                                                                                                      // ==
                                                                                                                      // axis??
 
+    }
+
+    /**
+     * 
+     * @param input     value to be modified (deadzoned)
+     * @param deadZone  the maximum value to be considered zero
+     * @return          the modified version of input
+     */
+    public double deadZones (double input, double deadZone) {
+        if (Math.abs(input) < deadZone){
+            return 0;
+        }
+        return input;
     }
 
     /**
