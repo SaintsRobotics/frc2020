080621647c34034834c9d887f818d025165ec85f
sony-programming <controlsystems@saintsrobotics.com>
2020-02-03 16:39:29 -0800

    Added interface for shooter

---
+++ code/src/test/java/frc/robot/mocks/subsystems/MockShooter.java
@@ -0,0 +1,59 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.mocks.subsystems;
+
+import frc.robot.common.ILogger;
+import frc.robot.common.IShooterSubsystem;
+import frc.robot.common.TraceableMockSubsystem;
+
+/**
+ * Add your docs here.
+ */
+public class MockShooter extends TraceableMockSubsystem implements IShooterSubsystem {
+
+    private boolean _isReady = false;
+
+    public MockShooter(ILogger logger) {
+        super(logger);
+        // TODO Auto-generated constructor stub
+    }
+    /*
+    * @param targetVelocity the rpm of shooter
+    */
+    @Override
+    public void startup(double targetVelocity) {
+        // TODO Auto-generated method stub
+        _isReady = true;
+    }
+
+    @Override
+    public boolean isReady() {
+        // TODO Auto-generated method stub
+        return _isReady;
+    }
+
+    @Override
+    public void enableShoot() {
+        // TODO Auto-generated method stub
+
+    }
+    @Override
+    public void disableShoot() {
+        // TODO Auto-generated method stub
+
+    }
+    @Override
+    public void shootBalls(int balls){
+        // TODO Auto-generated method stub
+    }
+    
+
+   
+
+
+}
