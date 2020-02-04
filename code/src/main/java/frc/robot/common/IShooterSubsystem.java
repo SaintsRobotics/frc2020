0d7c5695a683fbbbfff509fe26d00032887a8da7
noam987 <50681033+noam987@users.noreply.github.com>
2020-02-03 21:46:44 -0800

    Interface for Shooter (#33)
    Co-authored-by: Programming1899 <52844230+Programming1899@users.noreply.github.com>

---
+++ code/src/main/java/frc/robot/common/IShooterSubsystem.java
@@ -0,0 +1,51 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.common;
+
+/**
+ * Add your docs here.
+ */
+public interface IShooterSubsystem {
+
+    /**
+     * starts the shooter
+     * 
+     * @param targetVelocity the target velocity in rpm
+     */
+
+    void setSpeed(double targetVelocity);
+
+
+    /**
+     * 
+     * @return returns true if the shooter is at target velocity and is ready to
+     *         shoot
+     */
+    boolean isReady();
+
+    /**
+     * feeds the shooter when the shooter is up to speed until told to stop
+     */
+    void enableFeeding();
+
+    /**
+    * stops feeding the shooter balls
+    */
+    void disableFeeding();
+
+    /**
+    *
+    * @param balls number of balls shot
+    */
+    void shootBalls(int balls);
+    /**
+     * turns off the shooter motors
+     */
+    void stopShooter();
+
+}
