afcea12dfcc8b1c07b806ae7ed1ed9f7686c2804
Meghnath Dey <meghd2cx@gmail.com>
2020-02-08 14:34:58 -0800

    Fixed PR comment issues, fixed magic numbers

--- code/src/main/java/frc/robot/subsystems/Intake.java
+++ code/src/main/java/frc/robot/subsystems/Intake.java
@@ -1,27 +1,36 @@
 
+package frc.robot.subsystems;
+
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.controller.PIDController;
+import frc.robot.RobotConfig;
 import frc.robot.common.IIntakeSubsystem;
 import frc.robot.common.ILogger;
 import frc.robot.common.TraceableSubsystem;
 
 public class Intake extends TraceableSubsystem implements IIntakeSubsystem {
 
-    public SpeedController intakeController = new PWMVictorSPX(1);
-    public SpeedController armController = new PWMVictorSPX(2);
-    public Encoder armEncoder = new Encoder(1, 1);
+    private RobotConfig _config;
+    public SpeedController intakeController;
+    public SpeedController armController;
+    public Encoder armEncoder;
     private final PIDController m_armPIDController = new PIDController(0.1, 0, 0);
 
-    public Intake(final ILogger logger) {
+    public Intake(final ILogger logger, final RobotConfig config) {
         super(logger);
+        _config = config;
+        intakeController = new PWMVictorSPX(config.Intake.intakeControllerChannel);
+        armController = new PWMVictorSPX(config.Intake.armControllerChannel);
+        armEncoder = new Encoder(config.Intake.armEncoderChannelA, config.Intake.armEncoderChannelB);
 
         intakeController.setInverted(true);
         armController.setInverted(true);
         armEncoder.reset();
+
         // m_armPIDController.setS
 
         // Motor is inverted
@@ -31,11 +40,11 @@ public class Intake extends TraceableSubsystem implements IIntakeSubsystem {
         int count = armEncoder.get();
 
         // Based on the gear ratio of the motor
-        int PulsesPerRevolution;
+        int pulsesPerRevolution = 0;
 
         // Finding the pulses per revolution will then help find the number of pulses
         // needed to move a quarter of the distance (90deg) needed for the arm
-        int PulsesPerQuarter = PulsesPerRevolution / 4;
+        int pulsesPerQuarter = pulsesPerRevolution / 4;
 
         double distancePerPulse = armEncoder.getDistancePerPulse();
         // Arm is pwmVictorspx on port 2
@@ -46,21 +55,25 @@ public class Intake extends TraceableSubsystem implements IIntakeSubsystem {
     }
 
     public boolean isLowered() {
+
         boolean isArmStopped = armEncoder.getStopped();
+        // NOT Finished
+        // DEFAULT RETURN FALSE
+        return false;
 
     }
 
     public void spinIntake(boolean direction) {
 
-        double scaledvelocity;
+        double scaledVelocity = 0;
         if (direction) {
-            scaledvelocity = 1;
+            scaledVelocity = 1;
         }
         if (!direction) {
-            scaledvelocity = -1;
+            scaledVelocity = -1;
         }
 
-        intakeController.set(scaledvelocity);
+        intakeController.set(scaledVelocity);
 
     }
 
@@ -69,11 +82,6 @@ public class Intake extends TraceableSubsystem implements IIntakeSubsystem {
     }
 
     public boolean isSpinning() {
-        if (Math.abs(intakeController.get()) > 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return Math.abs(intakeController.get()) > 0;
     }
-
 }
