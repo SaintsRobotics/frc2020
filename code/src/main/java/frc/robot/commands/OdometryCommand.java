411452c4d205050a4c7fabc371ff6094f09afa3a
mihir-jain <mihirrjain200@gmail.com>
2020-02-17 20:01:35 -0800

    Renaming Stuff

--- code/src/main/java/frc/robot/commands/OdometryCommand.java
+++ code/src/main/java/frc/robot/commands/OdometryCommand.java
@@ -19,13 +19,10 @@ import frc.robot.common.*;
  * An example command that uses an example subsystem.
  */
 public class OdometryCommand extends TraceableCommand {
-    @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
     private final IDrivetrainSubsystem m_subsystem;
     private final Pose2d m_target;
-    private PIDController m_x;
-    private PIDController m_y;
-    private double m_xOutput;
-    private double m_yOutput;
+    private PIDController m_xPIDController;
+    private PIDController m_yPIDController;
 
     /**
      * Creates a new ExampleCommand.
@@ -33,21 +30,21 @@ public class OdometryCommand extends TraceableCommand {
      * @param subsystem The subsystem used by this command.
      */
     @Inject
-    public OdometryCommand(ILogger logger, IDrivetrainSubsystem subsystem, Pose2d target, RobotConfig coeffs) {
+    public OdometryCommand(ILogger logger, IDrivetrainSubsystem subsystem, Pose2d target, RobotConfig config) {
         super(logger);
         m_subsystem = subsystem;
         m_target = target;
 
-        m_x = new PIDController(coeffs.Odometry.pX, coeffs.Odometry.iX, coeffs.Odometry.dX);
-        m_y = new PIDController(coeffs.Odometry.pY, coeffs.Odometry.iY, coeffs.Odometry.dY);
+        m_xPIDController = new PIDController(config.Odometry.pX, config.Odometry.iX, config.Odometry.dX);
+        m_yPIDController = new PIDController(config.Odometry.pY, config.Odometry.iY, config.Odometry.dY);
 
-        m_x.setTolerance(coeffs.Odometry.toleranceX);
-        m_x.disableContinuousInput();
-        m_x.reset();
+        m_xPIDController.setTolerance(config.Odometry.toleranceX);
+        m_xPIDController.disableContinuousInput();
+        m_xPIDController.reset();
 
-        m_y.setTolerance(coeffs.Odometry.toleranceY);
-        m_y.disableContinuousInput();
-        m_y.reset();
+        m_yPIDController.setTolerance(config.Odometry.toleranceY);
+        m_yPIDController.disableContinuousInput();
+        m_yPIDController.reset();
         // Use addRequirements() here to declare subsystem dependencies.
         addRequirements(subsystem);
 
@@ -58,27 +55,27 @@ public class OdometryCommand extends TraceableCommand {
     @Override
     public void initialize() {
         super.initialize();
-        m_x.setSetpoint(m_target.getTranslation().getX());
-        m_y.setSetpoint(m_target.getTranslation().getY());
+        m_xPIDController.setSetpoint(m_target.getTranslation().getX());
+        m_yPIDController.setSetpoint(m_target.getTranslation().getY());
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        m_subsystem.move(m_x.calculate(m_subsystem.getCurrentPosition().getTranslation().getX()), 
-                m_y.calculate(m_subsystem.getCurrentPosition().getTranslation().getY()), 0, true);
+        m_subsystem.move(m_xPIDController.calculate(m_subsystem.getCurrentPosition().getTranslation().getX()), 
+                m_yPIDController.calculate(m_subsystem.getCurrentPosition().getTranslation().getY()), 0, true);
     }
 
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
-        m_x.reset();
-        m_y.reset();
+        m_xPIDController.reset();
+        m_yPIDController.reset();
     }
 
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        return m_x.atSetpoint() && m_y.atSetpoint();
+        return m_xPIDController.atSetpoint() && m_yPIDController.atSetpoint();
     }
 }
