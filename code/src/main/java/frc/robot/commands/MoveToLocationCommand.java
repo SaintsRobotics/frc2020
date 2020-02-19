f193a0cfbbd4cb8e013e7f008b62d2e445c24a62
noam987 <noam@arie11.com>
2020-02-18 22:03:18 -0800

    Added Move to location command

---
+++ code/src/main/java/frc/robot/commands/MoveToLocationCommand.java
@@ -0,0 +1,39 @@
+package frc.robot.commands;
+
+import com.google.inject.Inject;
+
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import frc.robot.RobotConfig;
+import frc.robot.common.IDrivetrainSubsystem;
+import frc.robot.common.ILogger;
+import frc.robot.common.TraceableCommand;
+
+public class MoveToLocationCommand extends TraceableCommand {
+    private Pose2d[] path;
+    private IDrivetrainSubsystem m_subsystem;
+    private OdometryCommand m_command;
+
+    @Inject
+    public MoveToLocationCommand(ILogger logger, IDrivetrainSubsystem subsystem, RobotConfig config, Pose2d position) {
+        super(logger);
+        addRequirements(m_subsystem);
+        m_subsystem = subsystem;
+        path[0] = position;
+        m_command = new OdometryCommand(logger, subsystem, path, config);
+
+    }
+
+    public void initialize() {
+        m_command.initialize();
+    }
+
+    public void execute() {
+        m_command.execute();
+    }
+
+    public boolean isFinished() {
+        return m_command.isFinished();
+    }
+}
