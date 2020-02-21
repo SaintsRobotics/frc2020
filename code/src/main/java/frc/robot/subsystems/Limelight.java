de3f87dabc647754162e41ea7171f750d98dfc92
mihir-jain <mihirrjain200@gmail.com>
2020-02-21 11:33:55 -0800

    Fixed the limelight code

--- code/src/main/java/frc/robot/subsystems/Limelight.java
+++ code/src/main/java/frc/robot/subsystems/Limelight.java
@@ -21,27 +21,57 @@ public class Limelight {
     m_limelight.getEntry("pipeline").setNumber(0);      
   }
 
-  public boolean ifTargetSeen(){
+  /**
+   * @return if it can see the target
+   */
+  public boolean isTargetSeen(){
     return m_limelight.getEntry("tv").getDouble(0.0) == 1.0;
   }
 
-  public void LEDState(int state){
+  /**
+   * @param int state
+   * 0 is the default value set in pipeline
+   * 1 is turning the LED's off
+   * 2 is forcing the LED's to blink
+   * 3 is turning the LED's on
+   */
+  public void setLEDState(int state){
     m_limelight.getEntry("ledMode").setNumber(state);
   }
 
+  /**
+   * @return number of the current pipeline
+   */
   public int getPipeline(){
     return (int) m_limelight.getEntry("getpipe").getNumber(0);
   }
 
+  /**
+   * @param int pipeline
+   * pipeline is the pipeline number you want to set it to
+   */
+  public void setPipeline(int pipeline){
+    m_limelight.getEntry("pipeline").setNumber(pipeline);
+  }
+
+  /**
+   * @return the angle offset from where the limelight is facing to the target
+   */
   public int getHeightAngularOffset(){
     return (int) m_limelight.getEntry("ty").getNumber(0);
   }
 
+  /**
+   * @return how far off the robot is turned from the target
+   */
   public int getRotationalOffset(){
     return (int) m_limelight.getEntry("tx").getNumber(0);
   }
 
-  public int getAreaOfScreenTakenByTheReflectiveTape(){
+  /**
+   * @return what percent of the limelight screen contains the target
+   */
+  public int getAreaOfScreenTakenByTarget(){
     return (int) m_limelight.getEntry("ta").getNumber(0);
   }
 }
