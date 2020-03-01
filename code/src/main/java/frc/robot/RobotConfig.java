12a1b657aee9d5b6585fb77a299df38ed2d67f13
HP Computer <programming@saintsrobotics.com>
2020-02-29 19:45:43 -0800

    added D to vision tracking pid, works really well

--- code/src/main/java/frc/robot/RobotConfig.java
+++ code/src/main/java/frc/robot/RobotConfig.java
@@ -131,9 +131,9 @@ public final class RobotConfig {
     }
 
     public final class Limelight {
-        public final double kP = .015;
+        public final double kP = .01;
         public final double kI = 0.00;
-        public final double kD = 0.0;
+        public final double kD = 0.05;
         public final double tolerance = 1.5;
 
         public final double angleSetpointDegrees = 0.0;
