018607cf5e9b81079fa1ec461a7965be1a959567
Keval Shah <captainrex493@gmail.com>
2020-11-08 12:39:15 -0800

    Add custom color for media

--- code/src/main/java/frc/robot/commands/SetAllianceColorCommand.java
+++ code/src/main/java/frc/robot/commands/SetAllianceColorCommand.java
@@ -4,6 +4,7 @@ import com.google.inject.Inject;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.util.Color;
 import frc.robot.common.ILedSubsystem;
 import frc.robot.common.ILogger;
 import frc.robot.common.TraceableCommand;
@@ -26,16 +27,20 @@ public class SetAllianceColorCommand extends TraceableCommand {
 
     public void initialize() {
         super.initialize();
+        Alliance alliance_to_send = Alliance.Blue;
+        this.ledSubsystem.setPreset(alliance);
     }
 
     public void execute() {
-        Alliance currentAlliance = DriverStation.getInstance().getAlliance();
-        if (alliance == currentAlliance) {
-            return;
-        } else {
-            alliance = currentAlliance;
-            ledSubsystem.setPreset(alliance);
-        }
+        // Alliance currentAlliance = DriverStation.getInstance().getAlliance();
+        // if (alliance == currentAlliance) {
+        // return;
+        // } else {
+        // alliance = currentAlliance;
+        // ledSubsystem.setPreset(alliance);
+        // }
+        Color color_to_set = new Color(0.0, 0.0, 0.0);
+        this.ledSubsystem.setColors(color_to_set, color_to_set);
 
     }
 
